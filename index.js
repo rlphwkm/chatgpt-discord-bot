c547145cdd65596523f5df2c25d3b742b6ecf0f5
Ralph <rlphwkm@gmail.com>
2022-12-24 00:54:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
 const Discord = require('discord.js');
 const openai = require('openai');
-const config = require("./config.json");
+const config = require('./config.json');
 
+// Set the OpenAI API key
 openai.apiKey = config.openaiapikey;
 
+// Create a new Discord client
 const client = new Discord.Client();
+
+// Login to Discord using the bot token
 client.login(config.bottokenkey);
 
+// Listen for messages in Discord
 client.on('message', message => {
-  if (message.content.startsWith('pingchatgpt')) {
-    message.channel.send('Pong! I am up and running.');
-  } else if (message.content.startsWith(config.botprefix)) {
-    const prompt = message.content.split(' ').slice(1).join(' ');
+  // Check if the message starts with the specified prefix
+  if (message.content.startsWith(config.botprefix)) {
+    // Get the prompt text by removing the prefix from the message
+    const prompt = message.content.slice(config.botprefix.length).trim();
 
+    // Send a request to the ChatGPT API with the prompt
     openai.completions.create({
-      prompt: prompt,
+      prompt,
       temperature: 0.5,
       max_tokens: 128,
       top_p: 1,
       frequency_penalty: 0,
       presence_penalty: 0
-    }).then(response => {
-      const chatgptResponse = response.choices[0].text;
-      message.channel.send(chatgptResponse);
-    }).catch(console.error);
+    })
+      .then(response => {
+        // Send the ChatGPT response back to the Discord channel
+        message.channel.send(response.choices[0].text);
+      })
+      .catch(console.error);
   }
 });
